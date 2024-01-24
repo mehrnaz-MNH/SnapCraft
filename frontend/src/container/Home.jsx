2fde9f20089454dc856bddbaa229b1c77ccf9d38
Mehrnaz(Marie) <88438830+mehrnaz-MNH@users.noreply.github.com>
2024-01-24 03:24:21 +0000

    s4-added assets and components and container

---
+++ frontend/src/container/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out">
+      <div className="hidden md:flex h-screen flex-initial"></div>
+      Home</div>
+  )
+}
+
+export default Home
